310e75b7e8e76663e24705eea74a69e33f5c44bb
Arsh Khan <khanarsh322@gmail.com>
2022-02-09 12:20:04 +0530

    Comments Update

--- mediaPipe.py
+++ mediaPipe.py
@@ -17,13 +17,10 @@ with mp_hands.Hands(
       # If loading a video, use 'break' instead of 'continue'.
       continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
 
-    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
@@ -46,13 +43,15 @@ with mp_hands.Hands(
           and (hand_landmarks.landmark[20].y < hand_landmarks.landmark[17].y):
           print("Paper")
           text = "Paper"
-        
+          
+        # Draw the hand annotations on the image.
         mp_drawing.draw_landmarks(
             image,
             hand_landmarks,
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
+        
     font = cv2.FONT_HERSHEY_SIMPLEX
     org = (0, 25)
     fontScale = 1
